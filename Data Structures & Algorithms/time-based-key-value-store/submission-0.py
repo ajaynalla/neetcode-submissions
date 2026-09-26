3e35ccbee17a9bdeda86b81e66ddfacb6c048b5f
Ajay Kumar Nalla <94650045+ajaynalla@users.noreply.github.com>
2026-09-26 18:38:10 -0400

    Update: time-based-key-value-store - submission-0

--- Data Structures & Algorithms/time-based-key-value-store/submission-0.py
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -1,23 +1,22 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = {} #key: list of [val, timestamp]
+        self.cap = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append([value, timestamp]) 
+        if key not in self.cap:
+            self.cap[key] = []
+        self.cap[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        values = self.store.get(key, [])
-
-        l, r = 0, len(values) - 1
+        arr = self.cap.get(key, [])
+        l, r = 0, len(arr) - 1
+        best = ""
         while l <= r:
-            m = (l+r) // 2
-            if values[m][1] <= timestamp: 
-                res = values[m][0]
-                l = m + 1
+            mid = (l+r)//2
+            if arr[mid][1] <= timestamp:
+                best = arr[mid][0]
+                l = mid + 1
             else:
-                r = m - 1
-        return res
+                r = mid - 1
+        return best
